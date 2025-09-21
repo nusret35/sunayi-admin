82ec1eabfa1e2d91d02cce6fe50e1c3f11988c8f
Nusret Ali Kızılaslan <nusret35@users.noreply.github.com>
2025-09-22 00:53:44 +0300

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["localhost", "encrypted-tbn0.gstatic.com", "127.0.0.1"],
+    domains: ["localhost", "encrypted-tbn0.gstatic.com", "127.0.0.1", "sunayi.com", "files.sunayi.com"],
     remotePatterns: [
       {
         protocol: "https",
