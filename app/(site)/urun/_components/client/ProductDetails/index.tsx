cca7a88851f8a0c16e267c0b95b89d09734926b0
Nusret Ali Kızılaslan <nusret35@users.noreply.github.com>
2025-09-22 00:26:04 +0300

    update

--- app/(site)/urun/_components/client/ProductDetails/index.tsx
+++ app/(site)/urun/_components/client/ProductDetails/index.tsx
@@ -1,20 +1,10 @@
 "use client";
 import { useGetPostQuery } from "@/redux/services/postApi";
-import SheetMetalProductDetails from "../SheetMetalProductDetails";
-import { SheetMetalCuttingPost } from "@/types/post";
 
 const ProductDetails = ({ id }: { id: string }) => {
   const { data, isLoading, isSuccess } = useGetPostQuery(id);
 
-  return (
-    <>
-      {data?.type === "sheet_metal_cutting" && (
-        <SheetMetalProductDetails
-          product={data?.post as SheetMetalCuttingPost}
-        />
-      )}
-    </>
-  );
+  return <></>;
 };
 
 export default ProductDetails;
